a94f56192b0254afb9f21add0f60d74275d287dd
Sanghyeon Lee <shlee@maklass.app>
2024-12-22 21:36:36 +0900

    폰트 변경 기능 추가 (Draft)

--- src/main/poe2launch.ts
+++ src/main/poe2launch.ts
@@ -24,7 +24,7 @@ export function poe2Launch(win: BrowserWindow, url: string): void {
     dialog
       .showMessageBox(win, {
         type: 'question',
-        buttons: [executeKakao64, `${executeKakao64} (글로벌 버전)`],
+        buttons: [executeKakao64, `${executeKakao64} (글로벌 버전)`, '게임 폰트 변경'],
         defaultId: 0,
         title: 'Path of Exile 2',
         message: '어떤 클라이언트로 실행할까요?'
@@ -39,6 +39,29 @@ export function poe2Launch(win: BrowserWindow, url: string): void {
           spawn(`${path.join(gamePath, executeKakao64)}`, {
             cwd: gamePath
           })
+        }
+        if (response.response === 2) {
+          // 폰트 선택창 표시
+          dialog
+            .showOpenDialog(win, {
+              properties: ['openFile'],
+              filters: [{ name: 'Fonts', extensions: ['ttf'] }],
+              title: 'Path of Exile 2 폰트 변경',
+              message: '적용하실 폰트를 선택하세요. 원복은 취소를 해주세요',
+              defaultPath: 'Z:\\home\\deck\\'
+            })
+            .then((result) => {
+              const fontPath = (result.filePaths && result.filePaths[0]) || ''
+              // 취소한 경우 폰트 원복 절차 진행
+              if (result.canceled || fontPath === '') {
+                // TODO: 폰트 원복
+                poe2RestoreFont(win)
+                return
+              }
+
+              // 폰트 변경 진행
+              poe2ChangeFont(win, fontPath)
+            })
         }
       })
   } else {
@@ -109,4 +132,86 @@ async function poe2Setup(win: BrowserWindow): Promise<void> {
 
     throw error
   }
+}
+
+function poe2ChangeFont(win: BrowserWindow, fontPath: string): void {
+  // 먼저 선택된 경로의 폰트를 C:\\Windows\\Fonts 폴더로 'poe2.ttf' 이름으로 복사
+  fs.copyFile(fontPath, 'C:\\Windows\\Fonts\\poe2.ttf', (error) => {
+    if (error) {
+      console.error('poe2ChangeFont::Copy:: ', error)
+      throw error
+    }
+
+    // 레지스트리 변경
+    // HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts
+    // "POE2 Launcher Font"="poe2.ttf"
+
+    const targetDir = app.getPath('temp')
+    console.log('Target Directory:', targetDir)
+
+    // targetDir 경로가 존재하지 않는 경우 폴더 생성
+    if (!fs.existsSync(targetDir)) {
+      fs.mkdirSync(targetDir, { recursive: true })
+    }
+
+    // 레지스트리 변경 스크립트 생성
+    const regScript = path.join(targetDir, 'poe2_font.reg')
+    fs.writeFileSync(
+      regScript,
+      'Windows Registry Editor Version 5.00\n\n' +
+        '[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts]\n' +
+        '"POE2 Launcher Font"="poe2.ttf"\n\n' +
+        '[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\FontSubstitutes]\n' +
+        '"Noto Sans CJK TC"="POE2 Launcher Font"\n' +
+        '"Spoqa Han Sans Neo"="POE2 Launcher Font"\n'
+    )
+
+    // 레지스트리 변경 스크립트 실행
+    spawn('regedit', ['/s', regScript])
+
+    // 완료 메시지 출력
+    dialog.showMessageBox(win, {
+      type: 'info',
+      title: 'Path of Exile 2',
+      message: '폰트 변경이 완료되었습니다. 런쳐를 재시작해주세요'
+    })
+  })
+}
+
+function poe2RestoreFont(win: BrowserWindow) {
+  // 레지스트리 항목 삭제
+  // HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts
+  // "POE2 Launcher Font" 삭제
+  // HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontSubstitutes
+  // "Noto Sans CJK TC" 및 "Spoqa Han Sans Neo" 삭제
+
+  const targetDir = app.getPath('temp')
+  console.log('Target Directory:', targetDir)
+
+  // targetDir 경로가 존재하지 않는 경우 폴더 생성
+  if (!fs.existsSync(targetDir)) {
+    fs.mkdirSync(targetDir, { recursive: true })
+  }
+
+  // 레지스트리 변경 스크립트 생성
+  const regScript = path.join(targetDir, 'poe2_font_restore.reg')
+  fs.writeFileSync(
+    regScript,
+    'Windows Registry Editor Version 5.00\n\n' +
+      '[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts]\n' +
+      '"POE2 Launcher Font"=-\n\n' +
+      '[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\FontSubstitutes]\n' +
+      '"Noto Sans CJK TC"=-\n' +
+      '"Spoqa Han Sans Neo"=-\n'
+  )
+
+  // 레지스트리 변경 스크립트 실행
+  spawn('regedit', ['/s', regScript])
+
+  // 완료 메시지 출력
+  dialog.showMessageBox(win, {
+    type: 'info',
+    title: 'Path of Exile 2',
+    message: '폰트 변경이 원복되었습니다. 런쳐를 재시작해주세요'
+  })
 }
