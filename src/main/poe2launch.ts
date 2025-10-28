da3447ad5709041835171c28ccdb94c4ec909159
psp3322 <psp3322@gmail.com>
2025-10-28 20:17:06 +0900

    Update poe2launch.ts
    poe1

--- src/main/poe2launch.ts
+++ src/main/poe2launch.ts
@@ -17,8 +17,8 @@ export function poe2Launch(win: BrowserWindow, url: string): void {
   console.log('Token:', token)
   console.log('User Code:', userCode)
 
-  const gamePath = 'C:\\Daum Games\\Path of Exile2'
-  const executeKakao64 = 'PathOfExile_x64_KG.exe'
+  const gamePath = 'C:\\Daum Games\\Path of Exile'
+  const executeKakao64 = 'PathOfExile_KG.exe'
 
   if (poe2IsInstalled(executeKakao64)) {
     dialog
@@ -26,7 +26,7 @@ export function poe2Launch(win: BrowserWindow, url: string): void {
         type: 'question',
         buttons: [executeKakao64, `${executeKakao64} (글로벌 버전)`],
         defaultId: 0,
-        title: 'Path of Exile 2',
+        title: 'Path of Exile',
         message: '어떤 클라이언트로 실행할까요?'
       })
       .then((response) => {
@@ -71,7 +71,7 @@ export function poe2Launch(win: BrowserWindow, url: string): void {
         // 완료 메시지 출력
         dialog.showMessageBox(win, {
           type: 'info',
-          title: 'Path of Exile 2',
+          title: 'Path of Exile',
           message: '설치 프로그램이 표시될것입니다. 설치 완료 후 게임 시작 버튼을 다시 클릭하세요'
         })
       })
@@ -79,7 +79,7 @@ export function poe2Launch(win: BrowserWindow, url: string): void {
         // 에러 메시지 출력
         dialog.showMessageBox(win, {
           type: 'error',
-          title: 'Path of Exile 2',
+          title: 'Path of Exile',
           message: '설치 중 오류가 발생했습니다. 다시 시도해주세요\n\n' + error.message
         })
       })
@@ -88,22 +88,22 @@ export function poe2Launch(win: BrowserWindow, url: string): void {
 
 function poe2IsInstalled(execute: string): boolean {
   // 기본 경로에서 설치되어 있는지 여부를 확인
-  // C:\Daum Games\Path of Exile2
-  const installPath = 'C:\\Daum Games\\Path of Exile2'
+  // C:\Daum Games\Path of Exile
+  const installPath = 'C:\\Daum Games\\Path of Exile'
 
   // 해당 경로에 execute 파일이 있는지 확인
   if (fs.existsSync(path.join(installPath, execute))) {
     return true
   }
 
-  console.log('Path of Exile 2 is not installed')
+  console.log('Path of Exile is not installed')
   return false
 }
 
 async function poe2Setup(win: BrowserWindow): Promise<void> {
-  // 다음 주소에서 클라이언트 설치파일 다운로드
-  // https://patch.poe2.kakaogames.com/kg_live/Game/poe2/Install/PathOfExile2_Setup.exe
-  const url = 'https://patch.poe2.kakaogames.com/kg_live/Game/poe2/Install/PathOfExile2_Setup.exe'
+  // 다음 주소에서 클라이언트 설치파일 다운로드, 1로 바꿈
+  // https://poe.gdn.gamecdn.net/kg_live/Game/poe/Install/PathOfExile_Setup.exe
+  const url = 'https://poe.gdn.gamecdn.net/kg_live/Game/poe/Install/PathOfExile_Setup.exe'
 
   const targetDir = app.getPath('temp')
   console.log('Target Directory:', targetDir)
@@ -116,11 +116,11 @@ async function poe2Setup(win: BrowserWindow): Promise<void> {
   try {
     await download(win, url, {
       directory: targetDir,
-      filename: 'PathOfExile2_Setup.exe'
+      filename: 'PathOfExile_Setup.exe'
     })
 
     // 다운로드 완료 후 실행
-    const setupPath = path.join(targetDir, 'PathOfExile2_Setup.exe')
+    const setupPath = path.join(targetDir, 'PathOfExile_Setup.exe')
     console.log('Setup Path:', setupPath)
     spawn(setupPath)
   } catch (error) {
@@ -215,3 +215,4 @@ async function poe2Setup(win: BrowserWindow): Promise<void> {
 //     message: '폰트 변경이 원복되었습니다. 런쳐를 재시작해주세요'
 //   })
 // }
+
